18c0655a46c67918628502ebd1904937d924dab7
Harmit2101 <vhmvaiwala@gmail.com>
2026-02-08 09:35:00 +0530

    fix(backend): add express request/response types to jobs route

--- server/routes/jobs.ts
+++ server/routes/jobs.ts
@@ -1,10 +1,10 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import Job from "../models/Job";
 
 const router = Router();
 
 // GET all jobs
-router.get("/", async (_req, res) => {
+router.get("/", async (_req: Request, res: Response) => {
   try {
     const jobs = await Job.find().sort({ createdAt: -1 });
     res.json(jobs);
